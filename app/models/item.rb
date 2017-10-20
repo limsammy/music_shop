6dac1443d7bb07dbfa59493a417c348b0460b7e3
limsammy1@gmail.com <limsammy1@gmail.com>
2017-10-20 10:18:06 -0700

    specified upload path

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,10 +8,10 @@ class Item < ApplicationRecord
 
   enum status: %w(active retired)
 
-  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }, :default_url => "https://heuft.com/upload/image/400x267/no_image_placeholder.png"
+  has_attached_file :image, path: 'images/:filename', styles: { small: "64x64", med: "100x100", large: "200x200" }, :default_url => "https://heuft.com/upload/image/400x267/no_image_placeholder.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :audio
+  has_attached_file :audio, path: 'audio/:filename'
   validates_attachment_content_type :audio, :content_type => /.*/
 
   def formatted_price
